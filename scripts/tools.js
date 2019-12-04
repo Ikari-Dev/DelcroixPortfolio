4c63d84acccee50ffb86a9ae14f1aac06476cf9b
fldelcroix <florian.delcroix982@gmail.com>
2019-12-04 10:11:37 +0100

    contact update

---
+++ scripts/tools.js
@@ -0,0 +1,17 @@
+var frontelement = document.querySelector('.front')
+frontelement.addEventListener('click', function(){
+
+
+});
+
+var backelement = document.querySelector('.back')
+backelement.addEventListener('click', function(){
+
+
+});
+
+var frameelement = document.querySelector('.frame')
+frameelement.addEventListener('click', function(){
+
+
+});
