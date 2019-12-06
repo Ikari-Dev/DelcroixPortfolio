9cf5da347502af1a0515dfb8408ed7621cdd9a9d
fldelcroix <florian.delcroix982@gmail.com>
2019-12-06 01:56:18 +0100

    about update

--- scripts/tools.js
+++ scripts/tools.js
@@ -1,17 +1,20 @@
-var frontelement = document.querySelector('.front')
-frontelement.addEventListener('click', function(){
-
-
-});
-
-var backelement = document.querySelector('.back')
-backelement.addEventListener('click', function(){
-
+const frontelement = document.querySelector("a.front")
+frontelement.addEventListener("click", function(){
+    //console.log('test')
+    document.querySelector("p.skill").innerHTML = "HTML5, CSS3(SASS), Javascript ES6"
 
 });
 
-var frameelement = document.querySelector('.frame')
-frameelement.addEventListener('click', function(){
+const backelement = document.querySelector("a.backend")
+backelement.addEventListener("click", function(){
+    //console.log('test2')
+    document.querySelector("p.skill").innerHTML = "PHP7"
 
+});
+
+const frameelement = document.querySelector("a.frame")
+frameelement.addEventListener("click", function(){
+    //console.log('test3')
+    document.querySelector("p.skill").innerHTML = "Bootstrap"
 
 });
