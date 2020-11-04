18838666534de6626a543ded2423a3123e6bb7b4
DelcroixFlorian <florian.delcroix98@gmail.com>
2020-11-04 22:59:09 +0100

    fix lang manager

--- scripts/tools.js
+++ scripts/tools.js
@@ -36,6 +36,7 @@ if(dataelement){
 
 const tools = document.querySelector("a.tools")
 if(tools){
+    
     tools.addEventListener("click", e => {
         
         document.querySelector("p.skill").innerHTML = "WebPack, Git, Linux, VSCode, Browser DevTools"
@@ -61,6 +62,96 @@ if(card){
         card.style.transform = "rotateY(0deg)"
         card.style.transform = "rotateX(0deg)"
 
+    });
+}
+
+
+const toggleLang = document.querySelector("a.facebook")
+if(toggleLang){
+    toggleLang.addEventListener("click", e => {
+        /*switch (toggleLang.innerHTML) {
+            case "FR":
+                toggleLang.innerHTML = "EN"
+                break;
+            
+            case "EN":
+                toggleLang.innerHTML = "JAP"
+                break;
+
+            case "JAP":
+                toggleLang.innerHTML = "FR"
+                break;
+        
+            default:
+                toggleLang.innerHTML = "FR"
+                break;
+        }*/
+        changeLang(toggleLang.innerHTML.toLocaleLowerCase)
+    })
+}
+
+function changeLang(filename){
+    let path = "../data/"
+    let ext = ".json"
+    let file = path.concat(toggleLang.innerHTML.toLocaleLowerCase(), ext)
+    fetch(file)
+    .then(response => response.json())
+    .then(json => {
+        console.log(json.home);
+        const data = document.querySelector("a.data");
+        if(data){
+            data.innerHTML = json.about.data
+        }
+
+        const tools = document.querySelector("a.tools");
+        if(tools){
+            tools.innerHTML = json.about.Tools
+        }
+
+        const skill = document.querySelector("p.skill");
+        if(skill){
+            skill.innerHTML = json.about.prefill
+        }
+
+        const titleinfo = document.querySelector("h3.titleinfo");
+        if(titleinfo){
+            titleinfo.innerHTML = json.about.subInfo
+        }
+
+        const info = document.querySelector("p.word");
+        if(info){
+            info.innerHTML = json.about.info
+        }
+
+        if(document.title == "School"){
+            const address = document.getElementsByClassName('address');
+            if(address){
+                address[0].innerHTML = json.school.licence.address
+                address[1].innerHTML = json.school.dut.address
+                address[2].innerHTML = json.school.sti2d.address
+            }
+
+            const intitule = document.getElementsByClassName('intitule');
+            if(intitule){
+                intitule[0].innerHTML = json.school.licence.name
+                intitule[1].innerHTML = json.school.dut.name
+                intitule[2].innerHTML = json.school.sti2d.name
+            }
+
+            const infos = document.getElementsByClassName('info');
+            if(infos){
+                infos[0].innerHTML = json.school.licence.info
+                infos[1].innerHTML = json.school.dut.info
+                infos[2].innerHTML = json.school.sti2d.info
+            }
+
+            const a = document.getElementsByClassName('lienformation');
+            if(a){
+                for(let i = 0; i < a.length ; i++) {
+                    a[i].innerHTML = json.school.licence.detail
+                }    
+            }
+        }
     });
 }
 
